dcaa3f9c47e15e047582d2540874003c2e551909
MrsBoadi <160177915+MrsBoadi@users.noreply.github.com>
2024-04-07 00:51:36 -0700

    commit

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const debug = require('debug')('app');
 const morgan = require('morgan');
 const path = require('path');
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 3000;
 const app = express();
-const sessionsRouter = require('./src/router/sessionsRouter');
+const sessionsRouter = require('./src/routers/sessionsRouter');
 const adminRouter = require('./src/routers/adminRouter');
 
 app.use(morgan('tiny'));
@@ -15,8 +15,8 @@ app.use(express.static(path.join(__dirname, '/public/')));
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
-app.use('/sessions', sessionsRouter)
-app.use('admin', adminRouter)
+app.use('/sessions', sessionsRouter);
+app.use('admin', adminRouter);
 
 app.get('/', (req, res) => {
     res.render('index', {title: 'Globomantics', data: ['a', 'b', 'c'] });
